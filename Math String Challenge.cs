da2959850dfc17dc1bf3774cb64a849381a2a42c
GBDiso81 <Gbdiso81@gmail.com>
2025-10-19 08:28:20 +0800

    Create Math String Challenge.cs

---
+++ Math String Challenge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+namespace Math_With_Strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string firstname = "Glen Mark";
+            //string lastname = " Diso";
+            //Console.WriteLine(firstname + lastname);
+            //Console.ReadLine();
+
+            //string name = "Glen Mark";
+            //int number = 23;
+            //Console.WriteLine(name + number);
+            //Console.ReadLine();
+
+            string  name = "Glen Mark birthdate is ";
+            int num = 5191981;
+            Console.WriteLine(name + num);
+            Console.ReadLine();
+
+
+        }
+    }
+}
