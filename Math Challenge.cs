c2304850cae7d39856f1f202156e2eace4913d72
GBDiso81 <Gbdiso81@gmail.com>
2025-10-19 08:00:32 +0800

    Create Math Challenge.cs

---
+++ Math Challenge.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace MathChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int num1 = 150;
+            //int num2 = 1000;
+            //int total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //int num3 = 2000;
+            //int num4 = 500;
+            //int difference = num3 - num4;
+            //Console.WriteLine(difference);
+            //Console.ReadLine();
+
+            //int num1 = 15;
+            //int num2 = 20;
+            //int product = num1 * num2;
+            //Console.WriteLine(product);
+            //Console.ReadLine();
+
+            //int num1 = 4000;
+            //int num2 = 50;
+            //int quotient = num1 / num2;
+            //Console.WriteLine(quotient);
+            //Console.ReadLine();
+
+
+
+        }
+    }
+}
