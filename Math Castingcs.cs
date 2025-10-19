d84bb81fab587af1452e33e259a987b6ac26e4bd
GBDiso81 <Gbdiso81@gmail.com>
2025-10-19 08:17:20 +0800

    Create Math Castingcs.cs

---
+++ Math Castingcs.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace Math_Casting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double num1 = 7.03230923;
+            //float num2 = 5.9f;
+            //float total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //double num1 = 7.03230923;
+            //float num2 = 5.9f;
+            //double total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //double num1 = 7.03230923;
+            //float num2 = 5.9f;
+            //float total = (float)num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //decimal num1 = 7.03230923m;
+            //int num2 = 5;
+            //decimal total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+            //decimal num1 = 7.03230923m;
+            //sbyte num2 = 5;
+            //decimal total = num1 + num2;
+            //Console.WriteLine(total);
+            //Console.ReadLine();
+
+
+        }
+    }
+}
