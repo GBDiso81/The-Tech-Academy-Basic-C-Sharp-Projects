7583f5dd0d5689dc843ddae3ce980854baf43a38
GBDiso81 <Gbdiso81@gmail.com>
2025-10-21 07:59:00 +0800

    Create ternary operator.cs

---
+++ ternary operator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace Ternary_Operator_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using ternary operator
+            Console.WriteLine("Welcome to AOL!");
+            Console.ReadKey();
+            Console.WriteLine("What is your Grade?");
+            int grade = Convert.ToInt32(Console.ReadLine());
+            string result = (grade >= 75) ? "You Passed! " : "Sorry... You've Failed.";
+            Console.WriteLine(result);
+            Console.ReadLine();
+
+        }
+    }
+}
