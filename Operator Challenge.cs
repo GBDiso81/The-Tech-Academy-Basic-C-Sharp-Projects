d8873dc5bad22f94a12a44b970dd9c91cf16de66
GBDiso81 <Gbdiso81@gmail.com>
2025-10-21 06:42:44 +0800

    Create Operator Challenge.cs

---
+++ Operator Challenge.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+
+namespace Operator_Challenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Football World Cup Series");
+            Console.ReadKey();
+            Console.WriteLine("Input Score of Team A :");
+            int teamA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input Score of Team B:");
+            int teamB = Convert.ToInt32(Console.ReadLine());
+
+            if (teamA != teamB) 
+             Console.WriteLine("One Team will move to the Playoff!");
+
+
+            else
+
+            Console.WriteLine("We are going to Overtime!");
+
+            Console.ReadLine();
+
+
+
+            
+
+        } 
+    }
+}
