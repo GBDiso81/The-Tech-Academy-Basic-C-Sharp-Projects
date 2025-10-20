5e025af2b6e63d1373fef9dd497d391271c61725
GBDiso81 <Gbdiso81@gmail.com>
2025-10-21 07:10:10 +0800

    Create greater than and less than or equal to.cs

---
+++ greater than and less than or equal to.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+namespace greater_than_lesser_than
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //utiliza the greater than operator
+            Console.WriteLine("Election Primary Requirement :");
+            Console.ReadKey();
+            Console.WriteLine("Enter your Age :");
+            int age = Convert.ToInt32(Console.ReadLine());
+            if (age > 17)
+            {
+                Console.WriteLine("You are allowed to vote!");
+                Console.ReadLine();
+            }
+
+            else
+                Console.WriteLine("You are not allowed to Vote.");
+            Console.ReadLine();
+
+
+            //utilize the use of less than or equal to operator
+            Console.WriteLine("Welcome to Drivers Licensing Department");
+            Console.ReadKey();
+            Console.WriteLine("Enter your Age :");
+            int age1 = Convert.ToInt32(Console.ReadLine());
+            if (age1 <= 15)
+            {
+                Console.WriteLine("You are not allowed to apply for Drivers License.");
+                Console.ReadLine();
+            }
+            else
+                Console.WriteLine("You can now submit needed requirements for new application!");
+                
+            Console.ReadLine();
+        }
+    }
+}
