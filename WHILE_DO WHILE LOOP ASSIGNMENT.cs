00b9885638336d76e850c23307b22e221b826fe7
GBDiso81 <Gbdiso81@gmail.com>
2025-10-28 07:02:23 +0800

    Update WHILE_DO WHILE LOOP ASSIGNMENT.cs

--- WHILE_DO WHILE LOOP ASSIGNMENT.cs
+++ WHILE_DO WHILE LOOP ASSIGNMENT.cs
@@ -1,183 +1,182 @@
 ﻿using System;
 
 
-namespace Switch_Challenge
+namespace WHILE_DO_WHILE_LOOP
 {
     class Program
     {
         static void Main(string[] args)
-
-        //THIS IS A WHILE LOOP
-        //{
-        //    Console.WriteLine("Welcome to Weekly Vehicle Coding");
-        //    Console.WriteLine("Press any key to continue...");
-        //    Console.ReadLine();
-        //    Console.WriteLine("Enter your vehicle plate ending number:");
-        //    int endingNum = Convert.ToInt32(Console.ReadLine());
-        //    bool isPlatenum = endingNum == 9;
-
-
-        //    while (!isPlatenum) 
-        //    {
-        //        switch (endingNum)
-        //        {
-        //            case 1:
-        //                Console.WriteLine("Your Vehicle is not coded during Monday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 2:
-        //                Console.WriteLine("Your Vehicle is not coded during Monday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 3:
-        //                Console.WriteLine("Your Vehicle is  not coded during Tuesday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 4:
-        //                Console.WriteLine("Your Vehicle is not coded during Tuesday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 5:
-        //                Console.WriteLine("Your Vehicle is not coded during Wednesday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 6:
-        //                Console.WriteLine("Your Vehicle is not coded during Wednesday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 7:
-        //                Console.WriteLine("Your Vehicle is not coded during Thursday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 8:
-        //                Console.WriteLine("Your Vehicle is not coded during Thursday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 9:
-        //                Console.WriteLine("Warning!! Your Vehicle is coded during Friday!");
-        //                isPlatenum = true;
-        //                break;
-        //            case 0:
-        //                Console.WriteLine("Your Vehicle is not coded during Friday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            default:
-        //                Console.WriteLine("Invalid Plate! But no coding on weekend...Happy Weekend!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
+        {
+            //THIS IS A WHILE LOOP
+            //{
+            //    Console.WriteLine("Welcome to Weekly Vehicle Coding");
+            //    Console.WriteLine("Press any key to continue...");
+            //    Console.ReadLine();
+            //    Console.WriteLine("Enter your vehicle plate ending number:");
+            //    int endingNum = Convert.ToInt32(Console.ReadLine());
+            //    bool isPlatenum = endingNum == 9;
 
 
-        //        }
+            //    while (!isPlatenum) 
+            //    {
+            //        switch (endingNum)
+            //        {
+            //            case 1:
+            //                Console.WriteLine("Your Vehicle is not coded during Monday!");
+            //                Console.ReadLine();
+            //                Console.WriteLine("Enter your vehicle plate ending number:");
+            //                endingNum = Convert.ToInt32(Console.ReadLine());
+            //                break;
+            //            case 2:
+            //                Console.WriteLine("Your Vehicle is not coded during Monday!");
+            //                Console.ReadLine();
+            //                Console.WriteLine("Enter your vehicle plate ending number:");
+            //                endingNum = Convert.ToInt32(Console.ReadLine());
+            //                break;
+            //            case 3:
+            //                Console.WriteLine("Your Vehicle is  not coded during Tuesday!");
+            //                Console.ReadLine();
+            //                Console.WriteLine("Enter your vehicle plate ending number:");
+            //                endingNum = Convert.ToInt32(Console.ReadLine());
+            //                break;
+            //            case 4:
+            //                Console.WriteLine("Your Vehicle is not coded during Tuesday!");
+            //                Console.ReadLine();
+            //                Console.WriteLine("Enter your vehicle plate ending number:");
+            //                endingNum = Convert.ToInt32(Console.ReadLine());
+            //                break;
+            //            case 5:
+            //                Console.WriteLine("Your Vehicle is not coded during Wednesday!");
+            //                Console.ReadLine();
+            //                Console.WriteLine("Enter your vehicle plate ending number:");
+            //                endingNum = Convert.ToInt32(Console.ReadLine());
+            //                break;
+            //            case 6:
+            //                Console.WriteLine("Your Vehicle is not coded during Wednesday!");
+            //                Console.ReadLine();
+            //                Console.WriteLine("Enter your vehicle plate ending number:");
+            //                endingNum = Convert.ToInt32(Console.ReadLine());
+            //                break;
+            //            case 7:
+            //                Console.WriteLine("Your Vehicle is not coded during Thursday!");
+            //                Console.ReadLine();
+            //                Console.WriteLine("Enter your vehicle plate ending number:");
+            //                endingNum = Convert.ToInt32(Console.ReadLine());
+            //                break;
+            //            case 8:
+            //                Console.WriteLine("Your Vehicle is not coded during Thursday!");
+            //                Console.ReadLine();
+            //                Console.WriteLine("Enter your vehicle plate ending number:");
+            //                endingNum = Convert.ToInt32(Console.ReadLine());
+            //                break;
+            //            case 9:
+            //                Console.WriteLine("Warning!! Your Vehicle is coded during Friday!");
+            //                isPlatenum = true;
+            //                break;
+            //            case 0:
+            //                Console.WriteLine("Your Vehicle is not coded during Friday!");
+            //                Console.ReadLine();
+            //                Console.WriteLine("Enter your vehicle plate ending number:");
+            //                endingNum = Convert.ToInt32(Console.ReadLine());
+            //                break;
+            //            default:
+            //                Console.WriteLine("Invalid Plate! But no coding on weekend...Happy Weekend!");
+            //                Console.ReadLine();
+            //                Console.WriteLine("Enter your vehicle plate ending number:");
+            //                endingNum = Convert.ToInt32(Console.ReadLine());
+            //                break;
 
-        //        Console.ReadLine();
+
+            //        }
+
+            //        Console.ReadLine();
 
 
-        
-        //THIS IS A DO WHILE LOOP
 
-        //{
-        //    Console.WriteLine("Welcome to Weekly Vehicle Coding");
-        //    Console.WriteLine("Press any key to continue...");
-        //    Console.ReadLine();
-        //    Console.WriteLine("Enter your vehicle plate ending number:");
-        //    int endingNum = Convert.ToInt32(Console.ReadLine());
-        //    bool isPlatenum = endingNum == 9;
+            //THIS IS A DO WHILE LOOP
 
-        //    do
-        //    {
-        //        switch (endingNum)
-        //        {
-        //            case 1:
-        //                Console.WriteLine("Your Vehicle is not coded during Monday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 2:
-        //                Console.WriteLine("Your Vehicle is not coded during Monday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 3:
-        //                Console.WriteLine("Your Vehicle is  not coded during Tuesday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 4:
-        //                Console.WriteLine("Your Vehicle is not coded during Tuesday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 5:
-        //                Console.WriteLine("Your Vehicle is not coded during Wednesday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 6:
-        //                Console.WriteLine("Your Vehicle is not coded during Wednesday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 7:
-        //                Console.WriteLine("Your Vehicle is not coded during Thursday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 8:
-        //                Console.WriteLine("Your Vehicle is not coded during Thursday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            case 9:
-        //                Console.WriteLine("Warning!! Your Vehicle is coded during Friday!");
-        //                isPlatenum = true;
-        //                break;
-        //            case 0:
-        //                Console.WriteLine("Your Vehicle is not coded during Friday!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //            default:
-        //                Console.WriteLine("Invalid Plate! But no coding on weekend...Happy Weekend!");
-        //                Console.ReadLine();
-        //                Console.WriteLine("Enter your vehicle plate ending number:");
-        //                endingNum = Convert.ToInt32(Console.ReadLine());
-        //                break;
-        //        }
-        //    }
-        //    while (!isPlatenum);
-        //            Console.ReadLine();
-            
-            
+            {
+                Console.WriteLine("Welcome to Weekly Vehicle Coding");
+                Console.WriteLine("Press any key to continue...");
+                Console.ReadLine();
+                Console.WriteLine("Enter your vehicle plate ending number:");
+                int endingNum = Convert.ToInt32(Console.ReadLine());
+                bool isPlatenum = endingNum == 9;
 
+                do
+                {
+                    switch (endingNum)
+                    {
+                        case 1:
+                            Console.WriteLine("Your Vehicle is not coded during Monday!");
+                            Console.ReadLine();
+                            Console.WriteLine("Enter your vehicle plate ending number:");
+                            endingNum = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 2:
+                            Console.WriteLine("Your Vehicle is not coded during Monday!");
+                            Console.ReadLine();
+                            Console.WriteLine("Enter your vehicle plate ending number:");
+                            endingNum = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 3:
+                            Console.WriteLine("Your Vehicle is  not coded during Tuesday!");
+                            Console.ReadLine();
+                            Console.WriteLine("Enter your vehicle plate ending number:");
+                            endingNum = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 4:
+                            Console.WriteLine("Your Vehicle is not coded during Tuesday!");
+                            Console.ReadLine();
+                            Console.WriteLine("Enter your vehicle plate ending number:");
+                            endingNum = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 5:
+                            Console.WriteLine("Your Vehicle is not coded during Wednesday!");
+                            Console.ReadLine();
+                            Console.WriteLine("Enter your vehicle plate ending number:");
+                            endingNum = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 6:
+                            Console.WriteLine("Your Vehicle is not coded during Wednesday!");
+                            Console.ReadLine();
+                            Console.WriteLine("Enter your vehicle plate ending number:");
+                            endingNum = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 7:
+                            Console.WriteLine("Your Vehicle is not coded during Thursday!");
+                            Console.ReadLine();
+                            Console.WriteLine("Enter your vehicle plate ending number:");
+                            endingNum = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 8:
+                            Console.WriteLine("Your Vehicle is not coded during Thursday!");
+                            Console.ReadLine();
+                            Console.WriteLine("Enter your vehicle plate ending number:");
+                            endingNum = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        case 9:
+                            Console.WriteLine("Warning!! Your Vehicle is coded during Friday!");
+                            isPlatenum = true;
+                            break;
+                        case 0:
+                            Console.WriteLine("Your Vehicle is not coded during Friday!");
+                            Console.ReadLine();
+                            Console.WriteLine("Enter your vehicle plate ending number:");
+                            endingNum = Convert.ToInt32(Console.ReadLine());
+                            break;
+                        default:
+                            Console.WriteLine("Invalid Plate! But no coding on weekend...Happy Weekend!");
+                            Console.ReadLine();
+                            Console.WriteLine("Enter your vehicle plate ending number:");
+                            endingNum = Convert.ToInt32(Console.ReadLine());
+                            break;
+                    }
+                }
+                while (!isPlatenum);
+                Console.ReadLine();
+
+            }
         }
     }
 }
