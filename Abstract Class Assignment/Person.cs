8efebcc39aecb79774be16a434f36d7427646ba5
GBDiso81 <Gbdiso81@gmail.com>
2025-12-07 10:51:17 +0800

    Abstract Class Assignment

---
+++ Abstract Class Assignment/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace AbstractClassAssignment
+{
+    //1. Create an abstract class called Person with two properties: string firstName and string lastName.
+    public abstract class Person
+    {
+        
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // 2.Give it the method SayName().
+        public abstract void SayName();
+    }
+}
---
+++ Abstract Class Assignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace AbstractClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 5.Instantiate an Employee object with firstName “Sample” and lastName “Student”. 
+            Employee employee = new Employee();
+
+            
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+
+            // Call the SayName() method on the object
+            employee.SayName();
+            Console.ReadLine();
+        }
+    }
+}
