0955bec660c71d7f40a1de07a91ea98588f028c7
GBDiso81 <Gbdiso81@gmail.com>
2025-12-09 06:37:23 +0800

    Parameters Assignment

---
+++ Parameters Assignment/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParametersAssignment
+{ 
+
+    //1. Make the Employee class take a generic type parameter.
+class Employee<T>
+    {
+        // 2. Add a property to the Employee class called “things” and have its data type be a generic list matching the generic type of the class.
+        public List<T> Things { get; set; }
+    }
+}
---
+++ Parameters Assignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ParametersAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3. Instantiate an Employee object with type "string" as its generic parameter.
+            // Assign a list of strings as the property value of Things.
+            Employee<string> stringEmployee = new Employee<string>();
+            stringEmployee.Things = new List<string> { "car", "cellphone", "laptop" };
+
+            // 4. Instantiate an Employee object with type "int" as its generic parameter.
+            // Assign a list of integers as the property value of Things.
+            Employee<int> intEmployee = new Employee<int>();
+            intEmployee.Things = new List<int> { 1, 2, 3, 4 };
+
+            // 5. Create a loop that prints all of the Things to the Console for the string employee
+            Console.WriteLine("String Employee Things:");
+            foreach (T thing in stringEmployee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+            Console.WriteLine("\nInt Employee Things:");
+            // Create a loop that prints all of the Things to the Console for the integer employee
+            foreach (T thing in intEmployee.Things)
+            {
+                Console.WriteLine(thing);
+            }
+
+        }
+    }
+}
