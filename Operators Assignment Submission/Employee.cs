cdbcea7723a51b0b305379caf9f87e2e171f5c19
GBDiso81 <Gbdiso81@gmail.com>
2025-12-09 02:44:37 +0800

    Operators Assignment Submission

---
+++ Operators Assignment Submission/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace OperatorsAssignment
+{
+    //1. Create an Employee class with Id, FirstName and LastName properties. 
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmpFirstName { get; set; }
+        public string EmpLastName { get; set; }
+
+        //2. In the Employee class, overload the “==” operator so it checks if two Employee objects are equal by comparing their Id property. 
+        public static bool operator ==(Employee employee1, Employee employee2)
+        {
+            return employee1.EmployeeId == employee2.EmployeeId;
+        }
+
+        // Overload in pairs the operator "==" with operator "!==" .
+        public static bool operator !=(Employee employee1, Employee employee2)
+        {
+           return !(employee1 == employee2);
+        }
+
+       
+    }
+}
---
+++ Operators Assignment Submission/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+
+namespace OperatorsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 3. Instantiate first objects of the Employee class and assign values to properties. 
+            Employee employee1 = new Employee
+            {
+                // Assign values to their properties.
+                EmployeeId = 1,
+                EmpFirstName = "Glen",
+                EmpLastName = "Diso"
+            };
+
+            // Instantiate second objects of the Employee class and assign values to properties.
+            Employee employee2 = new Employee
+            {
+                //Assign values to their properties.
+                EmployeeId = 1,
+                EmpFirstName = "Glen Mark",
+                EmpLastName = "Diso"
+            };
+
+            // Compare the two employee objects using the overloaded '==' operator
+            bool areEqual = (employee1 == employee2);
+
+            // Display the result of the equality comparison to the console
+            Console.WriteLine($"Is the EmployeeID equal [using (==) Operator]?   \n { areEqual}" );
+            Console.WriteLine("\nPress any key to continue...");
+            Console.ReadLine();
+
+            // Compare the two employee objects using the overloaded '!=' operator
+            bool areNotEqual = (employee1 != employee2);
+
+            // Display the result of the inequality comparison to the console
+            Console.WriteLine($"Is the EmployeeID not equal [using (!=) Operator]?  \n { areNotEqual}");
+            
+
+            // Keep the console window open
+            
+            Console.WriteLine("\n\nPress any key to continue...");
+            Console.ReadLine();
+        }
+    }
+}
