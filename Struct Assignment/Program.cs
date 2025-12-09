1146986cfd43a556ec8423d711666fcc3e3aa59a
GBDiso81 <Gbdiso81@gmail.com>
2025-12-10 02:26:21 +0800

    Struct Assignment

---
+++ Struct Assignment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static StructAssignment.StructNumber;
+
+namespace StructAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2. In the Main() method, create an object of data type Number and assign an amount to it.
+            // Declare a variable 'myPi' of the struct type 'Number'.
+            Number myPi = new Number();
+
+            // Assign a decimal value to the 'Amount' property using the 'm' suffix for decimal literals.
+            myPi.Amount = 3.1415926535m;
+
+            // 3. Print this amount to the console.
+            
+            Console.WriteLine($"Rounded approximation for the value of PI: {myPi.Amount}"); 
+
+            // Keep the console window open
+            Console.WriteLine("\nPress any key to exit."); //
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Struct Assignment/StructNumber.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructAssignment
+{
+    class StructNumber
+    {
+        // 1. Create a struct called Number and give it the property "Amount" of type decimal.
+        public struct Number
+        {
+            public decimal Amount { get; set; }
+        }
+    }
+}
